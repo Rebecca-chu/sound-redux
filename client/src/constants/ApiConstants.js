49cf78d35ccf0b45aecb198453fbd558c58ad7fa
Rebecca <rebecca.chu518@gmail.com>
2018-08-29 21:15:31 +0800

    Temporary use others client_id
    Register client_id from soundcloud is currently unavailable.
    Ref: https://developers.soundcloud.com/

--- client/src/constants/ApiConstants.js
+++ client/src/constants/ApiConstants.js
@@ -1,5 +1,5 @@
 const API_HOSTNAME = '//api.soundcloud.com';
-export const CLIENT_ID = 'f4323c6f7c0cd73d2d786a2b1cdae80c';
+export const CLIENT_ID = 'a281614d7f34dc30b665dfcaa3ed7505';
 
 const constructUrl = url => `${API_HOSTNAME}${url}${url.indexOf('?') === -1 ? '?' : '&'}client_id=${CLIENT_ID}`;
 
